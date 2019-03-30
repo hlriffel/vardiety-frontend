c930d6869dc070380d6f3ab2778b366b8205f55e
ThaliaRobertaSchulle <thalia.schulle@philips.com>
2019-03-30 12:52:24 -0300

    menu

--- src/routes.js
+++ src/routes.js
@@ -3,14 +3,16 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Login from './components/login';
 import Register from './components/register';
-import ChecklistShopping from './components/main/checklistShopping';
+import Main from './components/main';
+
+/*import ChecklistShopping from './components/main/checklistShopping';
 import GenerateCalendar from './components/main/generateCalendar';
 import InitialDiet from './components/main/initialDiet';
 import InvitePatient from './components/main/invitePatient';
 import ListPatient from './components/main/listPatients';
 import RegisterComponents from './components/main/registerComponents';
 import RegisterRestrictions from './components/main/registerRestrictions';
-import VisualizeCalendar from './components/main/viewCalendar';
+import VisualizeCalendar from './components/main/viewCalendar';*/
 
 const Routes = () => (
   <BrowserRouter>
@@ -18,6 +20,7 @@ const Routes = () => (
       <Route exact path="/" component={Login} />
       <Route path="/login" component={Login} />
       <Route path="/register" component={Register} />
+      <Route path="/main" component={Main} />
     </Switch>
   </BrowserRouter>
 );
