bf4cbab92fb973a9e5602127bcc10729ad4d163e
ThaliaRobertaSchulle <thalia.schulle@philips.com>
2019-06-02 21:39:35 -0300

    responsive nav.

--- src/pages/main/header/index.js
+++ src/pages/main/header/index.js
@@ -15,44 +15,47 @@ export default class Header extends Component {
   render() {
     return (
       <header className="fixed-top">
-        <Navbar bg="primary" variant="dark">
+        <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark">
           <LinkContainer to="/main">
             <Navbar.Brand>Vardiety</Navbar.Brand>
           </LinkContainer>
-          <Nav className="mr-auto">
-            <NavDropdown title="Cadastros" bg="primary">
-              <NavDropdown.Item>
-                <LinkContainer to="/main/register-restrictions">
-                 <Nav.Link><font color="black">Restrições</font></Nav.Link>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto">
+              <NavDropdown title="Cadastros" bg="primary">
+                <NavDropdown.Item>
+                  <LinkContainer to="/main/register-restrictions">
+                    <Nav.Link><font color="black">Restrições</font></Nav.Link>
+                  </LinkContainer>
+                </NavDropdown.Item>
+                <NavDropdown.Item>
+                  <LinkContainer to="/main/register-components">
+                    <Nav.Link><font color="black">Componentes</font></Nav.Link>
+                  </LinkContainer>
+                </NavDropdown.Item>
+                <NavDropdown.Item>
+                  <LinkContainer to="/main/register-nutrients">
+                    <Nav.Link><font color="black">Nutrientes X Componentes</font></Nav.Link>
+                  </LinkContainer>
+                </NavDropdown.Item>
+              </NavDropdown>
+              {
+                userService.userType === 'NUT' &&
+                <LinkContainer to="/main/patient-list">
+                  <Nav.Link>Lista de pacientes</Nav.Link>
                 </LinkContainer>
-              </NavDropdown.Item>
-              <NavDropdown.Item>
-              <LinkContainer to="/main/register-components">
-                <Nav.Link><font color="black">Componentes</font></Nav.Link>
-              </LinkContainer>
-              </NavDropdown.Item>
-              <NavDropdown.Item>
-              <LinkContainer to="/main/register-nutrients">
-                <Nav.Link><font color="black">Nutrientes X Componentes</font></Nav.Link>
-              </LinkContainer>  
-              </NavDropdown.Item>
-            </NavDropdown>
-            {
-              userService.userType === 'NUT' &&
-              <LinkContainer to="/main/patient-list">
-                <Nav.Link>Lista de pacientes</Nav.Link>
-              </LinkContainer>
-            }
-            {
-              userService.userType === 'PAT' &&
-              <LinkContainer to="/main/nutritionist-list">
-                <Nav.Link>Lista de nutricionistas</Nav.Link>
-              </LinkContainer>
-            }
-          </Nav>
-          <Form inline>
-            <Button variant="outline-light">Sair</Button>
-          </Form>
+              }
+              {
+                userService.userType === 'PAT' &&
+                <LinkContainer to="/main/nutritionist-list">
+                  <Nav.Link>Lista de nutricionistas</Nav.Link>
+                </LinkContainer>
+              }
+            </Nav>
+            <Form inline>
+              <Button variant="outline-light">Sair</Button>
+            </Form>
+          </Navbar.Collapse>
         </Navbar>
       </header>
     );
